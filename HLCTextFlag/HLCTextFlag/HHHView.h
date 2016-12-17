19602595674631faab993c4a57c49a313bcf2543
huanglch <416715288@qq.com>
2016-12-17 23:40:28 +0800

    4

---
+++ HLCTextFlag/HLCTextFlag/HHHView.h
@@ -0,0 +1,13 @@
+//
+//  HHHView.h
+//  HLCTextFlag
+//
+//  Created by huanglch on 2016/12/17.
+//  Copyright © 2016年 huanglch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HHHView : UIView
+
+@end
---
+++ HLCTextFlag/HLCTextFlag/HHHView.m
@@ -0,0 +1,21 @@
+//
+//  HHHView.m
+//  HLCTextFlag
+//
+//  Created by huanglch on 2016/12/17.
+//  Copyright © 2016年 huanglch. All rights reserved.
+//
+
+#import "HHHView.h"
+
+@implementation HHHView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
